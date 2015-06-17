0d8ad999728c6162b97ff720974f0c2bc22a2d9b
Krzysztof Urman <krzysztof.urman@espeo.pl>
2015-06-17 11:48:18 +0200

    Updates rest-service

--- rest-service/src/main/java/pl/espeo/avengers/endpoint/CORSFilter.java
+++ rest-service/src/main/java/pl/espeo/avengers/endpoint/CORSFilter.java
@@ -17,9 +17,9 @@ public class CORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, content-type, accept");
         chain.doFilter(req, res);
     }
 
