31220b33ae6fd2bdfef8d2085156db76e4bb18f6
Krzysztof Urman <krzysztof.urman@espeo.pl>
2015-06-17 11:49:10 +0200

    step-7: edit Avenger

--- step-7/app.js
+++ step-7/app.js
@@ -15,6 +15,11 @@
           controller: 'AvengersCtrl',
           controllerAs: 'list'
         })
+        .when('/avenger/:avenger/edit', {
+          templateUrl: 'tpl-avenger-edit',
+          controller: 'AvengerEditCtrl',
+          controllerAs: 'edit'
+        })
         .when('/avenger/:avenger', {
           templateUrl: 'tpl-avenger',
           controller: 'AvengerCtrl',
@@ -26,6 +31,7 @@
     })
     .controller('AvengersCtrl', AvengersCtrl)
     .controller('AvengerCtrl', AvengerCtrl)
+    .controller('AvengerEditCtrl', AvengerEditCtrl)
     .filter('checkmark', CheckMarkFilter);
 
   function CheckMarkFilter() {
@@ -38,16 +44,43 @@
   // Declaring our first controller
   function AvengersCtrl(AvengersRepo) {
     var vm = this;
+    vm.message = '';
     vm.avengers = AvengersRepo.query();
+
+    vm.avengers.$promise.catch(function () {
+      vm.message = "Unable to fetch Avengers Team";
+    });
   }
 
   function AvengerCtrl($routeParams, AvengersRepo, Assembly) {
     var vm = this;
-    vm.avenger = AvengersRepo.get({avengerId: $routeParams.avenger});
+    vm.message = '';
     vm.assemblyMe = assemblyMe;
+    fetchAvenger();
+
+
+    function fetchAvenger() {
+      vm.avenger = AvengersRepo.get({avengerId: $routeParams.avenger});
+      vm.avenger.$promise.catch(function () {
+        vm.message = 'No Avenger with id: ' + $routeParams.avenger;
+      });
+    }
 
     function assemblyMe(avenger) {
       Assembly.assemblyAvenger(avenger);
     }
   }
+
+  function AvengerEditCtrl($routeParams, $location, AvengersRepo) {
+    var vm = this;
+    vm.avenger = AvengersRepo.get({avengerId: $routeParams.avenger});
+    vm.updateAvenger = updateAvenger;
+
+    function updateAvenger() {
+      AvengersRepo.update(vm.avenger).$promise.then(function() {
+        $location.path("/avenger/" + vm.avenger.id);
+      });
+    };
+  }
+
 })();
--- step-7/repo-resource.js
+++ step-7/repo-resource.js
@@ -2,9 +2,13 @@
 
 (function () {
   angular.module('avengers.repo', ['ngResource'])
-      .service('AvengersRepo', AvengersRepo);
+    .service('AvengersRepo', AvengersRepo);
 
   function AvengersRepo($resource) {
-    return $resource('http://localhost:8080/bill/avengers/:avengerId', {avengerId: '@id'});
+    return $resource('http://localhost:8080/bill/avengers/:avengerId', {avengerId: '@id'},
+    {
+      'update': {method: 'PUT'}
+    }
+    );
   }
 })();
